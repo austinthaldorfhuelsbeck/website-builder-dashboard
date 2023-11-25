f3270a30bec3f0ef4b5d471965ca74cc3b245277
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2023-11-24 19:08:23 -0800

    data revisions

--- server/src/db/seeds/02-posts.ts
+++ server/src/db/seeds/02-posts.ts
@@ -1,12 +1,12 @@
 import { Knex } from "knex"
-import * as posts from "./00-posts.json"
+import posts from "./02-posts.json"
 
 async function seed(knex: Knex): Promise<void> {
 	// Deletes ALL existing entries
 	await knex("posts").del()
 
 	// Inserts seed entries
-	await knex.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE").then(() => {
+	await knex.raw("TRUNCATE TABLE posts RESTART IDENTITY CASCADE").then(() => {
 		return knex("posts").insert(posts)
 	})
 }
